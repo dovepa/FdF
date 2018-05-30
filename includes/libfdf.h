6b0b75fdb3a3cafd4882f70d2af996c031f584ff
Dove PALOMBO <dpalombo@e3r8p13.42.fr>
2018-05-30 16:23:09 +0200

    fin

--- includes/libfdf.h
+++ includes/libfdf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   libfdf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpalombo <dpalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/08 00:52:23 by dpalombo          #+#    #+#             */
-/*   Updated: 2018/05/10 16:22:24 by dpalombo         ###   ########.fr       */
+/*   Updated: 2018/05/30 15:58:26 by dpalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,20 @@
 #include "mlx.h"
 #include <math.h>
 
-int ft_line2(int xi, int yi, int xf, int yf, void **mlx_ptr, void **win_ptr, int color);
-void ft_ligne(int xi,int yi,int xf,int yf, void *mlx_ptr, void *win_ptr, int color);
-
-typedef	struct		s_dot
+typedef	struct		s_mlximg
 {
-	int				xa;
-	int				ya;
-	int				xb;
-	int				yb;
-	int				xc;
-	int				yc;
-	int				z;
-	struct s_list	*next;
-}					t_dot;
+	void			*img_ptr;
+	unsigned int	*data;
+	int				size_l;
+	int				bpp;
+	int				endian;
+}					t_mlximg;
 
 typedef	struct		s_windows
 {
-	void			*mlp_ptr;
+	void			*mlx_ptr;
 	void			*win_ptr;
+	t_mlximg		img;
 }					t_windows;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,25 +6,99 @@
 /*   By: dpalombo <dpalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/03 18:31:48 by dpalombo          #+#    #+#             */
-/*   Updated: 2018/05/17 11:00:34 by dpalombo         ###   ########.fr       */
+/*   Updated: 2018/05/30 16:22:50 by dpalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libfdf.h"
+#include "stdio.h"
+
+void ft_ligne(int xa,int ya,int xb,int yb, unsigned int *data, int win_width) 
+{
+	int dx;
+	int	dy;
+	int	i;
+	int xanc;
+	int	yanc;
+	int	cumul;
+	int	x;
+	int	y;
+	
+	x = xa ;
+	y = ya ;
+	dx = xb - xa ;
+	dy = yb - ya ;
+	xanc = ( dx > 0 ) ? 1 : -1 ;
+	yanc = ( dy > 0 ) ? 1 : -1 ;
+
+	dx = dx < 0 ? dx * -1 : dx;
+	dy = dy < 0 ? dy * -1 : dy;
+
+	data[x * win_width + y] = 0xFFFFFF;
+	if ( dx > dy )
+	{
+		cumul = dx / 2 ;
+  		for ( i = 1 ; i <= dx ; i++ )
+		{
+   			x += xanc ;
+   			cumul += dy ;
+   			if ( cumul >= dx )
+			{
+   				cumul -= dx ;
+   				y += yanc ;
+			}
+   			data[x * win_width + y] = 0xFFFFFF;
+		}
+	}
+    else
+	{
+		cumul = dy / 2 ;
+		for ( i = 1 ; i <= dy ; i++ )
+		{
+   			y += yanc ;
+		    cumul += dx ;
+		    if ( cumul >= dy )
+			{
+		        cumul -= dy ;
+		        x += xanc ;
+			}
+		data[x * win_width + y] = 0xFFFFFF;
+		}
+	}
+}
+
+void ft_dsqrt(int xa, int ya, int xb,int yb, int xc,int yc, unsigned int *data,int win_width)
+{
+	ft_ligne(xa, ya, xb, yb, data, win_width);
+	ft_ligne(xa, ya, xc, yc, data, win_width);
+	return ;
+}
+
 
 int	main(int argc, char **argv)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	t_windows windows;
+	int win_width = 800;
+	int win_height = 600;
 
+	
 	if (argc != 2)
 	{
 		ft_usage("./fdf exemple_of_maps");
 		return (1);
 	}
-	mlx_ptr = mlx_init();
-	mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, ft_strjoin("|--- 42 FdF ---| Map : ", argv[1]));
-	mlx_loop(mlx_ptr);
+	windows.mlx_ptr = mlx_init();
+	windows.win_ptr = mlx_new_window(windows.mlx_ptr, win_width, win_height, ft_strjoin("|--- 42 FdF ---| Map : ", argv[1]));
+	windows.img.img_ptr = mlx_new_image(windows.mlx_ptr, win_width, win_height);
+	windows.img.data = (unsigned int *)mlx_get_data_addr(windows.img.img_ptr, &windows.img.bpp, &windows.img.size_l,
+		&windows.img.endian);
+
+	ft_dsqrt(10, 10, 200, 200, 10, 200, windows.img.data, win_width);
+	
+	mlx_put_image_to_window(windows.mlx_ptr, windows.win_ptr, windows.img.img_ptr, 0, 0);
+	mlx_loop(windows.mlx_ptr);
 	return (0);
-}
+}	
+
+
+/*mlx_destroy_image(windows.mlx_ptr, windows.img.img_ptr);*/
