75a0b1d971038494b148834c5778d7723f03caf2
Unknow <unknow@Unknows-Mac.local>
2018-09-27 15:45:39 +0200

    Fin

--- src/event.c
+++ src/event.c
@@ -6,65 +6,77 @@
 /*   By: dpalombo <dpalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/25 14:12:15 by dpalombo          #+#    #+#             */
-/*   Updated: 2018/09/26 11:32:50 by dpalombo         ###   ########.fr       */
+/*   Updated: 2018/09/27 15:42:25 by dpalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libfdf.h"
 #include "mlx_rgb.c"
 
-//  static int ft_rcolor(t_windows *w)
+// int ft_mousemove(int x,int y, t_windows *w)
 // {
-// 	if (w->draw.rcolor == 1)
-// 	{
-// 		w->draw.colorn += 1;
-// 		if (w->draw.colorn > 751)
-// 			w->draw.colorn = 0;
-// 		sleep(3);
-// 		w->draw.color = mlx_col_name[w->draw.colorn].color;
-// 	}
-// 	else
-// 		w->draw.rcolor = 0;
-// 	ft_draw(w);
-// 	return (0); 
+// 	{w->draw.mx = 0;
+// 	w->draw.mx = x;
+// 	w->draw.my = y;
+// 	ft_draw(w);}
+// 	return (0);
 // }
 
- static int ft_rcolorbasic(t_windows *w)
+// int ft_mousedown(int button, int x,int y, t_windows *w)
+// {
+// 	(void *)x;
+// 	(void *)y;
+// 	(void *)w;
+// 	return (button);
+// }
+
+// int ft_mouseup(int button, int x,int y, t_windows *w)
+// {
+// 	(void *)x;
+// 	(void *)y;
+// 	(void *)w;
+// 	return (button);
+// }
+void ft_gradient(t_windows w)
 {
+	int r;
+	int g;
+	int b;
+
+	r = ((w->draw.color >> 16) & 11111111);
+	g = ((color >> 8) & 11111111);
+	b = (color & 11111111);
+	while (r < 255 || g < 255 || b < 255)
+	{
+		if (r < 255)
+			r++;
+		if (g < 255)
+			g++;
+		if (b < 255)
+			b++;
+
+	}
+	
+}
+
+int ft_color(t_windows *w)
+{
+	if (w->draw.rcolor == 1)
+	{
 		w->draw.colorn += 1;
 		if (w->draw.colorn > 751)
 			w->draw.colorn = 0;
 		w->draw.color = mlx_col_name[w->draw.colorn].color;
 		ft_draw(w);
-		return (0);
-}
-
-
-
-int ft_mousemove(int x,int y, t_windows *w)
-{
-	if (ft_mousedown == 1 && ft_mouseup == 0)
-	{w->draw.mx = 0;
-	w->draw.mx = x;
-	w->draw.my = y;
-	ft_draw(w);}
+		usleep(100000);
+	}
+	else
+	{
+		w->draw.rcolor = 0;
+		w->draw.color = RED;
+		ft_draw(w);
+	}
 	return (0);
-}
-
-int ft_mousedown(int button, int x,int y, t_windows *w)
-{
-	(void *)x;
-	(void *)y;
-	(void *)w;
-	return (button);
-}
-
-int ft_mouseup(int button, int x,int y, t_windows *w)
-{
-	(void *)x;
-	(void *)y;
-	(void *)w;
-	return (button);
 }
 
 int ft_key(int key, t_windows *w)
@@ -76,15 +88,8 @@ int ft_key(int key, t_windows *w)
 	}
 	if (key == C_COLOR)
 	{
-		// mlx_loop_hook(w->mlx_ptr, ft_rcolor, (void *)w);
-		// w->draw.rcolor++;
-		ft_rcolorbasic(w);
-	}
-	if (key == R_COLOR)
-	{
-		// mlx_loop_hook(w->mlx_ptr, ft_rcolor, (void *)w);
-		// w->draw.rcolor++;
-		mlx_clear_window(w->mlx_ptr, w->win_ptr);
+		mlx_loop_hook(w->mlx_ptr, ft_color, w);
+		w->draw.rcolor++;
 	}
 	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dpalombo <dpalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/03 18:31:48 by dpalombo          #+#    #+#             */
-/*   Updated: 2018/09/26 11:16:23 by dpalombo         ###   ########.fr       */
+/*   Updated: 2018/09/27 15:14:49 by dpalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,13 @@ void ft_draw(t_windows *w)
 	// ft_bresenham(w->draw.mx + 53,	w->draw.my + 4,		w->draw.mx + 4,		w->draw.my + 53, w->img.data, w->win_width,	w->draw.color);
 	// ft_bresenham(w->draw.mx + 1,	w->draw.my + 1,		w->draw.mx + 30,	w->draw.my + 30, w->img.data, w->win_width,	w->draw.color);
 	// ft_bresenham(w->draw.mx + 200,	w->draw.my + 200,	w->draw.mx + 100,	w->draw.my + 100, w->img.data, w->win_width,	w->draw.color);
+	int y = 0;
+	while (y < 300)
+	{
+		y++;
+		ft_bresenham(700,	150+y,	900,	150+y, w->img.data, w->win_width,	w->draw.color);
+
+	}
 	ft_bresenham(500,	400,	w->draw.mx,	w->draw.my, w->img.data, w->win_width,	w->draw.color);
 	mlx_put_image_to_window(w->mlx_ptr, w->win_ptr, w->img.img_ptr, 0, 0);
 	return ;
@@ -55,9 +62,9 @@ int	main(int argc, char **argv)
 	w.img.img_ptr = mlx_new_image(w.mlx_ptr, w.win_width, w.win_height);
 	w.img.data = (char *)mlx_get_data_addr(w.img.img_ptr, &w.img.bpp, &w.img.size_l, &w.img.endian);
 	mlx_key_hook(w.win_ptr, ft_key, (void *)&w);
-	mlx_hook(w.win_ptr, 6, 0, ft_mousemove, (void *)&w);
-	mlx_hook(w.win_ptr, 4, 0, ft_mousedown, (void *)&w);
-	mlx_hook(w.win_ptr, 5, 0, ft_mouseup, (void *)&w);
+	// mlx_hook(w.win_ptr, 6, 0, ft_mousemove, (void *)&w);
+	// mlx_hook(w.win_ptr, 4, 0, ft_mousedown, (void *)&w);
+	// mlx_hook(w.win_ptr, 5, 0, ft_mouseup, (void *)&w);
 	
 	ft_draw(&w);
 	mlx_loop(w.mlx_ptr);
